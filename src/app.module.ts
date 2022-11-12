41ab44797b2d2f3a110bbe6fc62177bf25d42810
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-11-12 13:36:14 -0500

    chore(general): add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/api-rest')],
   controllers: [AppController],
   providers: [AppService],
 })
