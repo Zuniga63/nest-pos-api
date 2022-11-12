7f77914dce2503e4eb26706b1eeb6cf933bf4635
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-11-12 13:49:46 -0500

    chore: add command for lint-staged

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-const useles;
-
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/api-rest')],
   controllers: [AppController],
