a731879556cb8044b4280f5e30a350e75045d37a
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-11-12 13:46:36 -0500

    revert: restore to las version

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const useles;
+
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/api-rest')],
   controllers: [AppController],
