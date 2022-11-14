b2f59a09120b0e189e164b7af28b9d66ab12790c
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-11-13 19:40:43 -0500

    chore: add global pipes, filter and set virtuals for models

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
+import mongoose from 'mongoose';
 import { AppModule } from './app.module';
+
 import swaggerConfig from './config/swagger.config';
+import validationConfig from './config/validationPipe.config';
+import { AllExceptionsFilter } from './utils/all-exceptions.filter';
+
+// *Add virtual for all models
+mongoose.set('toJSON', { virtuals: true });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +17,9 @@ async function bootstrap() {
   // Swagger COnfig
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe(validationConfig));
+  app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
 
   await app.listen(process.env.PORT || 3000);
 }
