3b1b6644fb76d497fd0ae558084509a6c7e038aa
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-11-23 09:18:33 -0500

    docs: add Users and  Roles tags

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,5 +4,7 @@ export default new DocumentBuilder()
   .setTitle('Bar and Restaurant')
   .setDescription('This is an API for managment a bar or a restaurant')
   .setVersion('1.0')
-  .addTag('Auth')
+  .addTag('Auth', 'End point for register and login')
+  .addTag('Users', 'Route for user administration')
+  .addTag('Roles', 'Routes for role administration')
   .build();
