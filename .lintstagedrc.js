2ddd61dc03eed24e441d4a2069a0996d94ca51d1
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-12-25 14:58:03 -0500

    fix: add path relative

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,22 @@
+const path = require('path');
+
 module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
 
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `pnpm eslint ${filenames.join(' ')}`,
-    `pnpm prettier --write ${filenames.join(' ')}`,
+    `pnpm eslint ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' ')}`,
+    `pnpm prettier --write ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' ')}`,
   ],
 
   // Prettify only Markdown and JSON files
   '**/*.(md|json)': (filenames) =>
-    `pnpm prettier --write ${filenames.join(' ')}`,
+    `pnpm prettier --write ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' ')}`,
 };
