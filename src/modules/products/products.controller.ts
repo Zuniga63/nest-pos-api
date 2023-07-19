9aa830aa047e01d1ddadd6d6d3fab6e92d003cf6
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-07-19 11:32:49 -0500

    docs: add Product Tag to documentation

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('products')
+@ApiTags('Products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
